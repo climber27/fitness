64acf7ec87d5197d2e81ac707021a40d6777aca9
David Dlima <daviddlima@Davids-MacBook-Pro.local>
2020-04-28 19:28:29 -0500

    gitignore fix

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Track and analyze personal fitness",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/daviddlima/fitness",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
